4fdf5f8fe3099167b0f3a08b6dbbc15102ab8220
theodtasia <theodoratasia@gmail.com>
2021-10-07 16:59:20 +0300

    home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import navbar from './components/navbar.vue';
+import store from './store';
 import firebase from 'firebase/app';
 import 'tailwindcss/tailwind.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserAlt,faBars, faMinusCircle} from "@fortawesome/free-solid-svg-icons";
+import {
+  
+  faFacebook,
+  faStackOverflow,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+library.add(faUserAlt, faBars, faMinusCircle, faFacebook, faStackOverflow, faGithub);
 
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 var firebaseConfig = {
     apiKey: 'AIzaSyD18RyCRqsdEzKg-h5wsla0kguOM7SHR2c',
     authDomain: 'assignment-47c5e.firebaseapp.com',
@@ -16,10 +29,12 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('navbar',navbar)
 Vue.config.productionTip = false;
 
 new Vue({
     router,
+    store,
     render: h => h(App),
 }).$mount('#app');
---
+++ src/views/Draw.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <navbar></navbar>
+  <h1 class="text-6xl font-normal leading-normal mt-4 mb-2 text-pink-800">Draw Page</h1>
+  <div class="grid grid-cols-2 gap-3 bg-gray-800">
+    
+    <div class="col-span-1"> 
+     <h2 class="text-2xl font-semibold leading-normal mt-4 mb-2 text-pink-800"> Your Bet</h2>
+      <div v-for="number in this.numbers" :key="number"> 
+            <button  v-on:click="takeValue(number)" type="submit"  class="bg-pink-800 hover:bg-blue-dark text-white font-bold h-12 w-12 my-3 py-2 px-4 rounded">{{number}}</button>
+            <button  v-on:click="deleteValue(number)" type="submit">  <font-awesome-icon :icon="['fas', 'minus-circle']" class="mx-2 text-yellow-700 fa-lg icon alt"/> </button>
+            <h1>{{ msg }}, your id is {{ id }}</h1>
+    </div>
+    </div>
+    
+    <div id="show" class="bg-gray-400"> 
+      <h2 class="text-2xl font-semibold leading-normal mt-4 mb-2 text-pink-800"> Draw </h2>
+     </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+        name: 'Draw',
+        data () {
+            return {
+                id: 0,
+                ndraw: []
+            }
+        },
+        created() {
+            this.id = this.$route.params.id;
+        },
+        methods: {
+          
+         getRandomNumberBetween: function (min,max)
+         {
+             return Math.floor(Math.random()*(max-min+1)+min);
+          } 
+        }
+    }
+</script>
